5e519c5fa01ec3e732fc62bad88e01a5ff1acd9d
tmdlsk <liusikai2019@ia.ac.cn>
2019-07-31 11:06:20 +0800

    first

---
+++ IntChar.java
@@ -0,0 +1,9 @@
+public class IntChar {
+    public static int x;
+    public static char b;
+    public static void main(String[] args){
+        System.out.println(x);
+        System.out.println(b);
+    }
+}
+
